24c6cec825019c2b33e22a0fc676803180d85cc6
kle1603 <bkhang160303@gmail.com>
2023-09-19 20:53:55 +0700

    UI

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import DateCheckButton from "./components/DateCheckButton/DateCheckButton";
+
+function App() {
+    return (
+        <div>
+            <DateCheckButton />
+        </div>
+    );
+}
+
+export default App;
