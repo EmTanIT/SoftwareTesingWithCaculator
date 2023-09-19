33e388c1971b7df94561e2ed8caf10d85f0c2547
minhquan291020032003@gmail.com <minhquan291020032003@gmail.com>
2023-09-19 20:49:29 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 // ===== Imports =====
+import overwriteResponseJSON from './middlewares/overwriteResponseJSON.js'
 import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
+import checkDate from './routes/checkDateTime.js'
 import indexRouter from './routes/index.js'
 
 // ===== Config =====
 const server = express()
 const PORT = process.env.PORT || 3000
-
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(overwriteResponseJSON)
 // ===== Middlewares =====
 server.use(cors())
 server.use(bodyParser.json())
 
 // ===== Routes =====
 server.use('/', indexRouter)
+server.use('/checkDateTime', checkDate)
 
 server.listen(PORT, () => {
     console.log(`Server is listening at PORT=${PORT}`)
